19fa8c9283e60d55bb983f7cec18691028e36186
craftwerk <pst@crftwrk.de>
2022-06-28 10:21:21 +0200

    Reformat php files

--- templates/sc-swiper-hero-fade.php
+++ templates/sc-swiper-hero-fade.php
@@ -26,120 +26,121 @@ Single items:
 
 
 // Hero Slider Shortcode
-add_shortcode( 'bs-swiper-hero-fade', 'bootscore_swiper_hero_fade' );
-function bootscore_swiper_hero_fade( $atts ) {
+add_shortcode('bs-swiper-hero-fade', 'bootscore_swiper_hero_fade');
+function bootscore_swiper_hero_fade($atts) {
 
-	ob_start();
-	extract( shortcode_atts( array (
-		'type' => 'post',
-		'order' => 'date',
-		'orderby' => 'date',
-		'posts' => -1,
-		'category' => '',
-        'post_parent'    => '',
-		'tax' => '',
-		'terms' => '',
-		'id' => ''
-	), $atts ) );
+  ob_start();
+  extract(shortcode_atts(array(
+    'type' => 'post',
+    'order' => 'date',
+    'orderby' => 'date',
+    'posts' => -1,
+    'category' => '',
+    'post_parent'    => '',
+    'tax' => '',
+    'terms' => '',
+    'id' => ''
+  ), $atts));
 
-	$options = array(
-		'post_type' => $type,
-		'order' => $order,
-		'orderby' => $orderby,
-		'posts_per_page' => $posts,
-		'category_name' => $category,
-        'post_parent' => $post_parent,
-	);
+  $options = array(
+    'post_type' => $type,
+    'order' => $order,
+    'orderby' => $orderby,
+    'posts_per_page' => $posts,
+    'category_name' => $category,
+    'post_parent' => $post_parent,
+  );
 
-	$tax = trim( $tax );
-	$terms = trim( $terms );
-	if ( $tax != '' && $terms != '' ) {
-		$terms = explode( ',', $terms );
-		$terms = array_map( 'trim', $terms );
-		$terms = array_filter( $terms );
-		$terms = array_unique( $terms );
-		unset( $options['category_name'] );
-		$options['tax_query'] = array( array(
-            'taxonomy' => $tax,
-            'field'    => 'name',
-            'terms'    => $terms,
-        ) );
-	}
+  $tax = trim($tax);
+  $terms = trim($terms);
+  if ($tax != '' && $terms != '') {
+    $terms = explode(',', $terms);
+    $terms = array_map('trim', $terms);
+    $terms = array_filter($terms);
+    $terms = array_unique($terms);
+    unset($options['category_name']);
+    $options['tax_query'] = array(array(
+      'taxonomy' => $tax,
+      'field'    => 'name',
+      'terms'    => $terms,
+    ));
+  }
 
-	if ( $id != '' ) {
-		$ids = explode( ',', $id );
-		$ids = array_map( 'intval', $ids );
-		$ids = array_filter( $ids );
-		$ids = array_unique( $ids );
-		$options['post__in'] = $ids;
-	}
+  if ($id != '') {
+    $ids = explode(',', $id);
+    $ids = array_map('intval', $ids);
+    $ids = array_filter($ids);
+    $ids = array_unique($ids);
+    $options['post__in'] = $ids;
+  }
 
-	$query = new WP_Query( $options );
-	if ( $query->have_posts() ) { ?>  
+  $query = new WP_Query($options);
+  if ($query->have_posts()) { ?>
 
 
-<!-- Swiper -->
-<div class="heroes-fade swiper-container swiper mb-4">
+    <!-- Swiper -->
+    <div class="heroes-fade swiper-container swiper mb-4">
 
-    <div class="swiper-wrapper">
+      <div class="swiper-wrapper">
 
-        <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+        <?php while ($query->have_posts()) : $query->the_post(); ?>
 
-        <div class="swiper-slide h-100 bg-dark">
+          <div class="swiper-slide h-100 bg-dark">
 
             <!-- Featured Image-->
             <?php the_post_thumbnail('full', array('class' => 'swiper-hero-img')); ?>
 
             <div class="position-absolute top-0 end-0 bottom-0 start-0">
 
-                <div class="container h-100 d-flex flex-column">
+              <div class="container h-100 d-flex flex-column">
 
-                    <div class="mt-auto text-white mb-5 text-center">
+                <div class="mt-auto text-white mb-5 text-center">
 
-                        <?php bootscore_category_badge(); ?>
+                  <?php bootscore_category_badge(); ?>
 
-                        <!-- Title -->
-                        <h2 class="blog-post-title">
-                            <a class="text-white" href="<?php the_permalink(); ?>">
-                                <?php the_title(); ?>
-                            </a>
-                        </h2>
+                  <!-- Title -->
+                  <h2 class="blog-post-title">
+                    <a class="text-white" href="<?php the_permalink(); ?>">
+                      <?php the_title(); ?>
+                    </a>
+                  </h2>
 
-                        <!-- Excerpt & Read more -->
-                        <div class="card-text">
-                            <?php the_excerpt(); ?>
-                        </div>
+                  <!-- Excerpt & Read more -->
+                  <div class="card-text">
+                    <?php the_excerpt(); ?>
+                  </div>
 
-                        <a class="read-more btn btn-light" href="<?php the_permalink(); ?>"><?php _e('Read more »', 'bootscore'); ?></a>
+                  <a class="read-more btn btn-light" href="<?php the_permalink(); ?>"><?php _e('Read more »', 'bootscore'); ?></a>
 
-                        <!-- Tags -->
-                        <?php bootscore_tags(); ?>
+                  <!-- Tags -->
+                  <?php bootscore_tags(); ?>
 
-                    </div>
                 </div>
+              </div>
 
             </div>
 
-        </div><!-- .swiper-slide -->
+          </div><!-- .swiper-slide -->
 
-        <?php endwhile; wp_reset_postdata(); ?>
+        <?php endwhile;
+        wp_reset_postdata(); ?>
 
-    </div> <!-- .swiper-wrapper -->
+      </div> <!-- .swiper-wrapper -->
 
-    <!-- Add Pagination -->
-    <div class="swiper-pagination"></div>
-    <!-- Add Arrows -->
-    <div class="swiper-button-next d-none d-lg-block"></div>
-    <div class="swiper-button-prev d-none d-lg-block"></div>
+      <!-- Add Pagination -->
+      <div class="swiper-pagination"></div>
+      <!-- Add Arrows -->
+      <div class="swiper-button-next d-none d-lg-block"></div>
+      <div class="swiper-button-prev d-none d-lg-block"></div>
 
 
-</div><!-- swiper-container -->
+    </div><!-- swiper-container -->
 
-<!-- Swiper End -->
+    <!-- Swiper End -->
 
 <?php $myvariable = ob_get_clean();
-	return $myvariable;
-	}	
+    return $myvariable;
+  }
 }
 
 // Hero Slider Shortcode End
