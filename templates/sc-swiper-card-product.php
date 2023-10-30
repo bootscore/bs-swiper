ca67bbca11b1f073b4e5db69bbb6eb1c7dace9db
craftwerk <pst@crftwrk.de>
2023-10-30 20:53:41 +0000

    Update sc-swiper-card-product.php

--- templates/sc-swiper-card-product.php
+++ templates/sc-swiper-card-product.php
@@ -14,13 +14,13 @@
  * [bs-swiper-card-product]
  *
  * Optional:
+ * category="cars, boats"    Will pull products matching these categories    (Default: '')
+ * id="1, 2, 3"              Will show products matching these ids           (Default: '')
  * posts="12"                Specify how many products will be shown         (Default: 12)
  * orderby="date"            Specify how products will be ordered by         (Default: date)
  * order="DESC"              Specify if products will be ordered ASC or DESC (Default: DESC)
  * featured="true"           Will pull featured products                     (Default: false)
- * outofstock="true"         Will show out of stock products                 (Default: false)
- * category="cars, boats"    Will pull products matching these categories    (Default: '')
- * id="1, 2, 3"              Will show products matching these ids           (Default: '')
+ * outofstock="false"        Will hide out of stock products                 (Default: true)
  *
 */
 
