2f8e928672136527dd7026a191e60b925755140c
craftwerk <pst@crftwrk.de>
2025-10-16 12:08:54 +0200

    Moved enqueue to an own file

--- main.php
+++ main.php
@@ -17,31 +17,11 @@ defined( 'ABSPATH' ) || exit;
 
 
 /**
- * Register styles and scripts
+ * Load required files
  */
-function swiper_scripts() {
-  // File paths
-  $swiper_js      = plugin_dir_path(__FILE__) . 'assets/js/swiper-bundle.min.js';
-  $swiper_init_js = plugin_dir_path(__FILE__) . 'assets/js/swiper-init.min.js';
-  $swiper_css     = plugin_dir_path(__FILE__) . 'assets/css/swiper-bundle.min.css';
-  $swiper_style   = plugin_dir_path(__FILE__) . 'assets/css/swiper-style.min.css';
+require_once('inc/enqueue.php');                 // Enqueue scripts and styles
 
-  // Versions based on file modification time
-  $swiper_js_ver      = file_exists($swiper_js) ? date('YmdHi', filemtime($swiper_js)) : false;
-  $swiper_init_js_ver = file_exists($swiper_init_js) ? date('YmdHi', filemtime($swiper_init_js)) : false;
-  $swiper_css_ver     = file_exists($swiper_css) ? date('YmdHi', filemtime($swiper_css)) : false;
-  $swiper_style_ver   = file_exists($swiper_style) ? date('YmdHi', filemtime($swiper_style)) : false;
 
-  // Enqueue scripts with versioning
-  wp_enqueue_script('swiper-min-js', plugins_url('/assets/js/swiper-bundle.min.js', __FILE__), [], $swiper_js_ver, true);
-  wp_enqueue_script('swiper-init-js', plugins_url('/assets/js/swiper-init.min.js', __FILE__), [], $swiper_init_js_ver, true);
-
-  // Enqueue styles with versioning
-  wp_enqueue_style('swiper-min-css', plugins_url('/assets/css/swiper-bundle.min.css', __FILE__), [], $swiper_css_ver);
-  wp_enqueue_style('swiper-style-css', plugins_url('/assets/css/swiper-style.min.css', __FILE__), [], $swiper_style_ver);
-}
-
-add_action('wp_enqueue_scripts', 'swiper_scripts');
 
 
 
